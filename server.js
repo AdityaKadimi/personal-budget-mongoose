37cef02b85b47d3ab926a2b8491a3deece67ca9f
AdityaKadimi <adityakadimi@gmail.com>
2020-10-30 23:37:19 -0400

    Added Mongoose

--- server.js
+++ server.js
@@ -8,24 +8,68 @@ var url = "mongodb://localhost:27017/mongo_personal";
 var budgetModel=require('./models/budgetData');
 const loadData = require("./LoadData");
 const router = express.Router()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('',express.static('public'));
 
 
 mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology:true})
         .then(()=>{
             app.use(express.json())
-            app.use("/api",routes)
+            // app.use("/api",routes)
         })
         .catch((connectionError)=>{
           console.log(connectionError)
         })
 		
-app.use(cors());
 
 
-app.get('/budget',(req,res) => {
-    res.json(loadData);
-});
+
+// app.get('/budget',(req,res) => {
+//     res.json(loadData);
+// });
+
+app.get('/budget',(req,res)=>{
+    mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
+            .then(()=>{
+                console.log("Connection to database");
+                budgetModel.find({})
+                           .then((data)=>{
+                               console.log(data);
+                               res.status(200).send(data);
+                               mongoose.connection.close();
+                           })
+                           .catch((err)=>{
+                               console.log(err);
+                               res.status(500).send();
+                           })
+            })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at:+ http://localhost:${port}`);
+});
+
+// new post route to simulate data addition
+app.post('/budget',(req,res)=>{
+    console.log("inside post");
+    console.log(req.body);
+    let data = {id: req.body._id, title: req.body.title, budget: req.body.budget, color: req.body.color}
+    mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
+            .then(()=>{
+                console.log("Connection to the database is established");
+                budgetModel.insertMany(data,(err,data)=>{
+                    if(err){
+                        console.log(err);      
+                        res.send(err);
+                        mongoose.connection.close();
+                    }else{
+                        console.log("insert successful"); 
+                        res.send(data);    
+                        mongoose.connection.close();
+                    }                    
+                })                              
+})
 });
