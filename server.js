f0f4e4943be4cd07e677dc626f7f717f0a4ca5db
AdityaKadimi <adityakadimi@gmail.com>
2020-10-30 21:41:18 -0400

    Added Mongoose

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+var mongoose=require("mongoose");
+const cors= require('cors')
+const app = express();
+const routes=require("./routes");
+const port = 3000;
+var url = "mongodb://localhost:27017/mongo_personal";
+var budgetModel=require('./models/budgetData');
+const loadData = require("./LoadData");
+const router = express.Router()
+
+
+mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology:true})
+        .then(()=>{
+            app.use(express.json())
+            app.use("/api",routes)
+        })
+        .catch((connectionError)=>{
+          console.log(connectionError)
+        })
+		
+app.use(cors());
+
+
+app.get('/budget',(req,res) => {
+    res.json(loadData);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at:+ http://localhost:${port}`);
+});
